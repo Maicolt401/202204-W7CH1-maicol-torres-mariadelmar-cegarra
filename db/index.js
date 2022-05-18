cba1dfc55a4e3ad5dde24789d1deb2a46bdbbdef
MariaDelMar23 <maria.cegarra2303@gmail.com>
2022-05-18 20:33:53 +0200

    Created connectDB function

---
+++ db/index.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async (urlDB) =>
+  new Promise((resolve, reject) => {
+    mongoose.connect(urlDB, (error) => {
+      if (error) {
+        debug(chalk.red(`An error has occured: ${error.msg}`));
+        reject();
+        return;
+      }
+
+      debug(chalk.green("Connected to database"));
+      resolve();
+    });
+  });
+
+module.exports = connectDB;
