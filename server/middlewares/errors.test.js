9edf9be9a222b148d2328831c56d3ef3d139457c
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-05-20 01:18:49 +0200

    Tested general error custom error case

---
+++ server/middlewares/errors.test.js
@@ -0,0 +1,23 @@
+const { generalError } = require("./errors");
+
+describe("Given the generalError middleware", () => {
+  describe("When it receives an error with an statusCode 403 and customMessage 'Incorrect Username or password' and a res", () => {
+    test("Then it should call res' status and json methods with 403 and { msg : 'Incorrect Username or password'} respectively", () => {
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const error = {
+        statusCode: 403,
+        customMessage: "Incorrect Username or password",
+      };
+      const expectedStatusCode = 403;
+      const expectedJSON = { msg: "Incorrect Username or password" };
+
+      generalError(error, null, res);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+      expect(res.json).toHaveBeenCalledWith(expectedJSON);
+    });
+  });
+});
