006f13f37539e5b41d8997ddd2ba26443f7972fd
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-05-20 01:22:44 +0200

    Tested generalError with a general error

--- server/middlewares/errors.test.js
+++ server/middlewares/errors.test.js
@@ -20,4 +20,24 @@ describe("Given the generalError middleware", () => {
       expect(res.json).toHaveBeenCalledWith(expectedJSON);
     });
   });
+
+  describe("When it receives an error with an status 500 and a message and a res", () => {
+    test("Then it should call res' status and json methods with 500 and { msg : 'General error'} respectively", () => {
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const error = {
+        status: 500,
+        message: "TypeError: Cannot read properties of undefined",
+      };
+      const expectedStatusCode = 500;
+      const expectedJSON = { msg: "General error" };
+
+      generalError(error, null, res);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+      expect(res.json).toHaveBeenCalledWith(expectedJSON);
+    });
+  });
 });
